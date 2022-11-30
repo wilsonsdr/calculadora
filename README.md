66fca80be72e048782652f5d664a809555c87862
Wilson Souto dos Reis <wilsonsdr@gmail.com>
2022-11-29 23:40:32 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+    <img src="https://user-images.githubusercontent.com/81364355/204692884-a9a10d8f-d529-4e5f-b739-69f12caaa683.png" width="400">
+</div>
+
+# Descrição do projeto
+
+Este projeto foi o último desafio desenvolvido durante o 7 Days of Code da Alura, tinha como objetivo criar uma calculadora, durante os 7 dias pude reforçar e desenvolver novos conhecimentos e habilidades, em que irá me agregar em futuros projetos e desafios
---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+//#region função para inserir o número pressionado
+function inserir(numero) {
+  document.querySelector(".digitacao").innerHTML += numero;
+}
+//#endregion
+//#region função para limpar o campo
+function limparCampo() {
+  document.querySelector(".digitacao").innerHTML = "";
+  document.querySelector(".resultado").innerHTML = "";
+}
+//#endregion
+//#region função para realizar os calcúlos
+function calcular() {
+  const resultado = document.querySelector(".digitacao").innerHTML;
+
+  if (resultado) {
+    document.querySelector(".resultado").innerHTML = eval(resultado);
+  } else {
+    document.querySelector(".resultado").innerHTML = "Calculo inválído";
+  }
+}
+//#endregion
+//#region função mudar de cor
+function temaClaro() {
+  document.querySelector(".calculadora-container").classList.toggle("claro");
+  document.querySelector(".cabecalho").classList.toggle("claro");
+  document.querySelector(".resultado-container").classList.toggle("claro");
+  document.querySelector(".digitacao-container").classList.toggle("claro");
+  document.querySelector(".botoes").classList.toggle("claro");
+  document.querySelector(".fa-solid").classList.toggle("claro");
+
+  const botao = document.querySelectorAll(".botao");
+
+  for (botoes in botao) {
+    botao[botoes].classList.toggle("claro");
+  }
+}
+//#endregion
